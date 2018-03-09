30e174e12cc25ff38de6bed3d31ae6e4c65df0df
Owais Zahid <owais.zahid@gmail.com>
2018-03-09 23:43:32 +0800

    [OZ] adding unit tests

--- src/ReviewManager.js
+++ src/ReviewManager.js
@@ -1,33 +1,32 @@
 'use strict';
 
 class ReviewManager {
-    constructor(env, docClient) {
-      this.env = env;
-      this.docClient = docClient;
-    }
+	constructor(env, docClient) {
+		this.env = env;
+		this.docClient = docClient;
+	}
 
-    addDocument(bucket, fileName) {
-      let table = this.env.tableName || 'ReviewList';
-      let params = {
-          TableName: table,
-          Item:{
-              'bucketSrc': bucket,
-              'docId': fileName
-          }
-      };
-      console.log(params);
+	addDocument(bucket, fileName) {
+		let table = this.env.tableName || 'ReviewList';
+		let params = {
+			TableName: table,
+			Item:{
+				'bucketSrc': bucket,
+				'docId': fileName
+			}
+		};
 
-      return new Promise((resolve, reject) => {
-          this.docClient.put(params, function(err, data) {
-            if (err) {
-                console.log(err);
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-      });
-    }
+		return new Promise((resolve, reject) => {
+			this.docClient.put(params, function(err, data) {
+				if (err) {
+					console.log(err);
+					reject(err);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
 }
 
 module.exports = ReviewManager;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const ReviewManager = require('./ReviewManager.js');
-const AWS = require("aws-sdk");
+const AWS = require('aws-sdk');
 // Setting region is necessary for SAM-local testing
 AWS.config.update({region: 'us-west-2'});
 const DocumentClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
 exports.handler = (event, context, callback) => {
-    let srcBucket = event.Records[0].s3.bucket.name;
-    let srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-    let reviewManager = new ReviewManager(process.env, DocumentClient);
+	let srcBucket = event.Records[0].s3.bucket.name;
+	let srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+	let reviewManager = new ReviewManager(process.env, DocumentClient);
 
-    reviewManager.addDocument(srcBucket, srcKey).then((data) => {
-      callback(null, JSON.stringify(data, null, 2));
-    }, (error) => {
-      callback("Unable to add item.", JSON.stringify(error, null, 2));
-    });
+	reviewManager.addDocument(srcBucket, srcKey).then((data) => {
+		callback(null, JSON.stringify(data, null, 2));
+	}, (error) => {
+		callback('Unable to add item.', JSON.stringify(error, null, 2));
+	});
 };
